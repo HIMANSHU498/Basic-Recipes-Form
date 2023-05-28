74b9d231bcb54fc6e6d6be12345d0d5399e94f26
HIMANSHU498 <KUSHWAHHIMANSHU498@GMAIL.COM>
2023-05-28 15:36:14 +0530

    app complete

--- index.js
+++ index.js
@@ -3,24 +3,27 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const ejs = require("ejs");
 dotenv.config();
 app.use(bodyParser.urlencoded({ extended: "false" }));
 app.use(express.static("./public"));
+app.set("view engine", "ejs");
 const Recipes = mongoose.model("recipes", {
   recipeName: String,
   recipeTime: String,
-  ingredients: Array,
+  ingredients: [],
   serves: String,
 });
-app.get("", (req, res) => {
-  res.send("Welcome to food recipes");
-});
-app.get("/recipes", (req, resp) => {
+
+app.get("", (req, resp) => {
   Recipes.find()
-    .then((item) => resp.json(item))
+    .then((item) => resp.render("Form", { item }))
     .catch((err) => resp.json({ err: "something went wrong" }));
 });
-app.post("/recipes", (req, resp) => {
+app.get("/newrecipes", (req, res) => {
+  res.render("newRecipe");
+});
+app.post("/newrecipes", (req, resp) => {
   const { recipeName, recipeTime, ingredients, serves } = req.body;
   const recipes = new Recipes({
     recipeName: recipeName,
@@ -30,23 +33,37 @@ app.post("/recipes", (req, resp) => {
   });
   recipes
     .save()
-    .then(() => resp.json("Recipes saved successfully"))
+    .then(() => resp.redirect("/"))
     .catch((err) => resp.json({ err: "something went wrong" }));
 });
-app.put("/recipes/:id", (req, resp) => {
+app.get("/:id/edit", async (req, resp) => {
+  const { id } = req.params;
+  // const { recipeName, recipeTime, ingredients, serves } = req.body;
+  const recipe = await Recipes.findById(id);
+
+  Recipes.findByIdAndUpdate(id)
+    .then(resp.render("editRecipe", { recipe }))
+    .catch((err) => resp.json({ err: "something went wrong" }));
+});
+app.post("/:id/edit", (req, resp) => {
   const { id } = req.params;
   const { recipeName, recipeTime, ingredients, serves } = req.body;
 
-  Recipes.findByIdAndUpdate(id, { recipeName: recipeName })
-    .then((item) => resp.json(item))
+  Recipes.findByIdAndUpdate(id, {
+    recipeName: recipeName,
+    recipeTime: recipeTime,
+    ingredients: ingredients,
+    serves: serves,
+  })
+    .then(resp.redirect("/"))
     .catch((err) => resp.json({ err: "something went wrong" }));
 });
-app.delete("/recipes/:id", (req, resp) => {
+
+app.post("/:id/delete", (req, resp) => {
   const { id } = req.params;
-  const { recipeName, recipeTime, ingredients, serves } = req.body;
 
   Recipes.findByIdAndDelete(id)
-    .then((item) => resp.json(item))
+    .then(resp.redirect("/"))
     .catch((err) => resp.json({ err: "something went wrong" }));
 });
 app.listen(process.env.PORT, () => {
